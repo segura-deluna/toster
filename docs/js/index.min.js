1e89df25c8025813bb760b83f234be1deed9800f
Segura <segura-deluna@mail.ru>
2024-02-19 02:07:45 +0300

    add modal window

--- docs/js/index.min.js
+++ docs/js/index.min.js
@@ -35,6 +35,21 @@ spoiler.addEventListener('click', e => {
     content.style.maxHeight = '';
   }
 });
+;// CONCATENATED MODULE: ./src/js/modules/modal.js
+const modal = document.querySelector('.modal');
+const btn = document.querySelector('#myBtn');
+const span = document.querySelector('.modal__close');
+btn.onclick = function () {
+  modal.style.display = 'block';
+};
+span.onclick = function () {
+  modal.style.display = 'none';
+};
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+};
 ;// CONCATENATED MODULE: ./src/js/index.js
 
 
---
+++ src/js/modules/modal.js
@@ -0,0 +1,17 @@
+const modal = document.querySelector('.modal');
+const btn = document.querySelector('#myBtn');
+const span = document.querySelector('.modal__close');
+
+btn.onclick = function () {
+  modal.style.display = 'block';
+};
+
+span.onclick = function () {
+  modal.style.display = 'none';
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+};
